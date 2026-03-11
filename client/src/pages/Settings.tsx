6e34b6c424542034eef4123e44702f9a68d9e4c7
Rafael Garcez <rgs.dba@gmail.com>
2026-03-11 13:56:23 -0300

    fix: importar useMemo em Settings

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link } from "wouter";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
